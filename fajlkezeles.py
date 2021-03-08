e5ee2bced2915341f4d379c4c572d8f6d8f8a648
MiklosAprad <miklosarpad2007@gmail.com>
2021-03-08 11:49:39 +0100

    file kiiratás

--- fajlkezeles.py
+++ fajlkezeles.py
@@ -17,3 +17,19 @@ while True:
     if len(egy_sor) ==0:
         break
     print(egy_sor)
+
+
+# bármilyen ojjektum tárolása
+
+import pickle
+
+ez_egy_lista = ["gázlómadár", 999, False]
+file1 = open("tároló.dat", "wb")   # eddig ugyanaz
+pickle.dump(ez_egy_lista, file1)   # elrakta
+
+del  ez_egy_lista                  # itt törölte
+file1 = open("tároló.dat", "rb")
+ez_egy_masik_lista = pickle.load(file1)
+file1.close()
+
+print(ez_egy_masik_lista)
